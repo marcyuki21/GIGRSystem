28a326667110fa88ecb73d4c7813e9ec3a3e267d
akzellaroya <162241077+akzellaroya@users.noreply.github.com>
2024-03-05 10:58:36 +0800

    GI Interface with database
    GI Interface with database

--- GIGRSystem/GoodsIncoming.Designer.cs
+++ GIGRSystem/GoodsIncoming.Designer.cs
@@ -72,6 +72,7 @@
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
@@ -81,6 +82,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel2
@@ -131,7 +133,7 @@
             this.tabPage1.Controls.Add(this.lvlweigh);
             this.tabPage1.Controls.Add(this.label3);
             this.tabPage1.Controls.Add(this.label2);
-            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.panel3);
             this.tabPage1.Font = new System.Drawing.Font("Artifakt Element Book", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tabPage1.Location = new System.Drawing.Point(4, 32);
             this.tabPage1.Name = "tabPage1";
@@ -277,10 +279,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Artifakt Element", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(528, 20);
+            this.label1.Font = new System.Drawing.Font("Artifakt Element Heavy", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(488, 1);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(298, 37);
+            this.label1.Size = new System.Drawing.Size(400, 49);
             this.label1.TabIndex = 0;
             this.label1.Text = "Goods Incoming Form";
             // 
@@ -522,6 +525,15 @@
             this.button6.Text = "Search";
             this.button6.UseVisualStyleBackColor = true;
             // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.DarkOrange;
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Location = new System.Drawing.Point(-4, 2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1310, 55);
+            this.panel3.TabIndex = 22;
+            // 
             // GoodsIncoming
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -534,6 +546,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "GoodsIncoming";
             this.Text = "GoodsIncoming";
+            this.Load += new System.EventHandler(this.GoodsIncoming_Load);
             this.panel1.ResumeLayout(false);
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
@@ -546,6 +559,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.groupBox1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -596,5 +611,6 @@
         private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel3;
     }
 }
--- GIGRSystem/GoodsIncoming.cs
+++ GIGRSystem/GoodsIncoming.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+
 
 namespace GIGRSystem
 {
@@ -16,7 +19,13 @@ namespace GIGRSystem
         Class1 nc = new Class1();
         public static string gimodel;
 
-
+        public MySqlConnection mycon;
+        public MySqlCommand mycom;
+        public MySqlDataReader myread;
+        public MySqlDataAdapter myadapt;
+        public string connectionFurukawa = "Server=192.168.4.136;Port=3309;Database=furukawa;User ID=root;Password=123;";
+        public string connectionFormal = "Server=192.168.4.136;Port=3309;Database=formal;User ID=root;Password=123;";
+        public string connection137 = "Server=192.168.4.137;Port=3306;Database=mmios;User ID=root;Password=pass1234;";
 
         public GoodsIncoming()
         {
@@ -28,10 +37,70 @@ namespace GIGRSystem
             Environment.Exit(Environment.ExitCode);
         }
 
+        private void GoodsIncoming_Load(object sender, EventArgs e)
+        {
+            loadmodel();
+            loadhpnumber();
+        }
+
+        public void loadmodel()
+        {
+            string query = "select distinct model from mmiosmodel";
+            try
+            {
+                mycon = new MySqlConnection(connection137);
+                mycon.Open();
+                mycom = new MySqlCommand(query, mycon);
+                myread = mycom.ExecuteReader();
+                while (myread.Read())
+                {
+                    cbModel.Items.Add(myread.GetValue(0).ToString());
+
+
+                }
+                mycon.Close();
+                mycon.Dispose();
+            }
+            catch (Exception)
+            {
+                mycon.Close();
+                mycon.Dispose();
+                throw;
+            }
+        }
+        public void loadhpnumber()
+        {
+            string query = "select distinct hpnumber from mmiosmodel where model='"+cbModel.Text+"' ";
+            try
+            {
+                mycon = new MySqlConnection(connection137);
+                mycon.Open();
+                mycom = new MySqlCommand(query, mycon);
+                myread = mycom.ExecuteReader();
+                while (myread.Read())
+                {
+                    cbModel.Items.Add(myread.GetValue(0).ToString());
+
+                }
+                mycon.Close();
+                mycon.Dispose();
+            }
+            catch (Exception)
+            {
+                mycon.Close();
+                mycon.Dispose();
+                throw;
+            }
+        }
+
+
+
         private void cbModel_SelectedIndexChanged(object sender, EventArgs e)
         {
-            string query = "select ";
+            string query = "select model from mmiosmodel where model ='"+cbModel.Text+"' ";
             gimodel = nc.getdata(query);
         }
+
+
     }
 }
